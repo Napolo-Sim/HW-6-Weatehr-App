95a06181fd5331373cc3f4bbd6c92748c9c2b171
Napoleon Vuong <napolo101@gmail.com>
2020-05-08 20:29:07 -0700

    First commit -LATE

---
+++ app.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+    // var userSearch = "";
+    var apiKey = "44dcd9557c70cf145a53f5245ad9a8da";
+
+    $("#searchBtn").on("click", function (event) {
+        event.preventDefault();
+        userInput = $("#userSearch").val();
+        console.log(userInput);
+        $("#userSearch").val("");
+        $("#weatherInfo").html("");
+
+        $.ajax({
+            type: "GET",
+            //Don't forget to add https:// to all of your API URL's
+            url: `https://api.openweathermap.org/data/2.5/weather?q=Atherton&appid=44dcd9557c70cf145a53f5245ad9a8da`,
+            dataType: "json",
+        }).then(function (response) {
+            console.log(response);
+
+            // var weatherData = response.main.temp;
+            // console.log(weatherData);
+
+            // var tempFahrenheit = (weatherData - 273.15) * 9 / 5 + 32;
+            // console.log(tempFahrenheit);
+
+            // var tempFWhole = Math.round(tempFahrenheit);
+            // console.log(tempFWhole + " Degrees Fahrenheit");
+
+            // $("#weatherInfo").append(
+            //     `<h1>${tempFWhole} Degrees Fahrenheit</h1>`
+            // )
+        })
+    })
+})
+
